e5fac9d91421aa12c20887852338d76d03b716c2
Kevin Ananda Putra <anandakevinputra208@gmail.com>
2023-10-25 15:22:39 +0700

    Basic routing dan method HTTP #2

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users', (req, res) => {
+  res.send('Got All Users')
+})
+
+app.post('/user', (req, res) => {
+    res.send('Got a POST request')
+})
+
+app.put('/user/:id', (req, res) => {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user:id', (req, res) => {
+    res.send('Got a DELETE request at /user')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
