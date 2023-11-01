47389b575022c89553c10bb8cfcc57db287c56c1
Kevin Ananda Putra <anandakevinputra208@gmail.com>
2023-11-01 13:03:00 +0700

    Controller #9

--- router/users.js
+++ router/users.js
@@ -1,67 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-let users = [
-    {id: 1, nama:"Kevin", email: "anandakevinputra208@gmail.com"},
-    {id: 2, nama:"Zaneth", email: "checilonazaneth@gmail.com"},
-]
+const usercontroller = require('../controller/user')
 
-router.get('/users', (req, res) => {
-    if(users.length > 0){
-        res.json({
-            status: true,
-            data: users,
-            method: req.method,
-            url: req.url
-        })
-    } else {
-        res.json({
-            status: false,
-            message: "Data masih kosong"
-        })
-    }
-  })
+router.get('/users', usercontroller.index)
   
-  router.post('/user', (req, res) => {
-      users.push(req.body)
-      res.json({
-        status: true,
-        data: users,
-        method: req.method,
-        url: req.url,
-        message: "Data berhasil ditambahkan"
-    })
-  })
+  router.post('/user', usercontroller.store)
   
-  router.put('/user/:id', (req, res) => {
-      const id = req.params.id
-      users.filter(user => {
-        if(user.id == id){
-            user.nama = req.body.nama
-            user.email = req.body.email
-            return user
-        }
-      })
-      res.json({
-        status: true,
-        data: users,
-        method: req.method,
-        url: req.url,
-        message: "Data berhasil diupdate"
-    })
-  })
+  router.put('/user/:id', usercontroller.update)
   
-  router.delete('/user/:id', (req, res) => {
-      const id = req.params.id
-      users = users.filter(user => user.id != id)
-
-      res.json({
-        status: true,
-        data: users,
-        method: req.method,
-        url: req.url,
-        message: "Data berhasil dihapus"
-    })
-  })
+  router.delete('/user/:id', usercontroller.delete)
 
 module.exports = router
