0c49cf7b24d8f317c71ab51596b2469cfa86157e
Kevin Ananda Putra <anandakevinputra208@gmail.com>
2023-11-08 15:23:59 +0700

    Koneksi Database MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const userrouter = require('./router/users')
+const connectDB = require('./config/db')
 
 const port = 3000
 
@@ -12,6 +13,8 @@ app.get('/', (req, res) => {
 })
 
 app.use(userrouter)
+
+connectDB()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
